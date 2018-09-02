b35f87618bb37361a6f67cfab23e65c61d2a07db
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-09-03 09:24:06 +1000

    Request in guard is undefined

--- sample/12-graphql-apollo/src/app.module.ts
+++ sample/12-graphql-apollo/src/app.module.ts
@@ -7,6 +7,7 @@ import { CatsModule } from './cats/cats.module';
     CatsModule,
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
+      // context: ({req}) => ({...req}),
     }),
   ],
 })
--- sample/12-graphql-apollo/src/cats/cats.guard.ts
+++ sample/12-graphql-apollo/src/cats/cats.guard.ts
@@ -5,6 +5,9 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export class CatsGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const ctx = GqlExecutionContext.create(context);
+    console.log('Request in context is -> ', context.switchToHttp().getRequest());
+    console.log('Request in gql-executioncontext is -> ', ctx.switchToHttp().getRequest());
+
     return true;
   }
 }
